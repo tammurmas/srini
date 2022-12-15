b252498c25dd2c2b5b744aadfc9347dca974cce3
Urmas Tamm <urmas.tamm@xolo.io>
2022-12-15 18:24:18 +0200

    initial project structure

---
+++ src/main/java/org/tamm/srini/SriniApplication.java
@@ -0,0 +1,13 @@
+package org.tamm.srini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SriniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SriniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tamm/srini/SriniApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tamm.srini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
